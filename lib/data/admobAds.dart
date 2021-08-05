a6304a1975f499787341fa341a8f72317803c76a
neeraj <neerajkedhar@gmail.com>
2021-08-06 00:51:42 +0530

    day a6-1

--- lib/data/admobAds.dart
+++ lib/data/admobAds.dart
@@ -1,16 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-class Ads extends StatefulWidget {
-  const Ads({Key? key}) : super(key: key);
+class Ads {
+  InterstitialAd? _interstitialAd;
+  int _numInterstitialLoadAttempts = 0;
+  void _createInterstitialAd() {
+    InterstitialAd.load(
+        adUnitId: InterstitialAd.testAdUnitId,
+        request: AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (InterstitialAd ad) {
+            print('$ad loaded');
+            _interstitialAd = ad;
+            _numInterstitialLoadAttempts = 0;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('InterstitialAd failed to load: $error.');
+            _numInterstitialLoadAttempts += 1;
+            _interstitialAd = null;
+            if (_numInterstitialLoadAttempts <= 4) {
+              _createInterstitialAd();
+            }
+          },
+        ));
+  }
 
-  @override
-  _AdsState createState() => _AdsState();
-}
-
-class _AdsState extends State<Ads> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+  void _showInterstitialAd() {
+    if (_interstitialAd == null) {
+      print('Warning: attempt to show interstitial before loaded.');
+      return;
+    }
+    _interstitialAd!.fullScreenContentCallback = FullScreenContentCallback(
+      onAdShowedFullScreenContent: (InterstitialAd ad) =>
+          print('ad onAdShowedFullScreenContent.'),
+      onAdDismissedFullScreenContent: (InterstitialAd ad) {
+        print('$ad onAdDismissedFullScreenContent.');
+        ad.dispose();
+        _createInterstitialAd();
+      },
+      onAdFailedToShowFullScreenContent: (InterstitialAd ad, AdError error) {
+        print('$ad onAdFailedToShowFullScreenContent: $error');
+        ad.dispose();
+        _createInterstitialAd();
+      },
+    );
+    _interstitialAd!.show();
+    _interstitialAd = null;
   }
 }
