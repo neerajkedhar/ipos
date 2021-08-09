a5807661d3746a15461819a7bccb3daeb58c4aac
neeraj <neerajkedhar@gmail.com>
2021-08-09 23:45:00 +0530

    daya9-4

---
+++ lib/screens/news.dart
@@ -0,0 +1,79 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:ipos/data/uicolors.dart';
+import 'package:ipos/getData.dart';
+import 'package:ipos/widgets/newswidget.dart';
+
+class News extends StatefulWidget {
+  const News({Key? key}) : super(key: key);
+
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  late Color background;
+  late Color foreground;
+  late Color accent;
+  late Color mainText;
+  late Color subText;
+  AppColors colors = AppColors();
+  GetNews cats = new GetNews();
+  SetNews setnews = new SetNews();
+
+  var newsList;
+  Future getHttp() async {
+    try {
+      var moneyresponse = await Dio().get(
+          'https://newsapi.org/v2/everything?q=ipos%20news%20in%20india&sortBy=publishedAt&apiKey=746a076d37f241f2acf59c8d6920ed75');
+      var mcnewsData = moneyresponse.data['articles'];
+      newsList = mcnewsData;
+      print(newsList);
+    } catch (e) {
+      print(e);
+    }
+    // getFromListedIPOFireStore();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    background = Theme.of(context).brightness == Brightness.dark
+        ? colors.darkBG
+        : colors.liteBG;
+    foreground = Theme.of(context).brightness == Brightness.dark
+        ? colors.darkFG
+        : colors.liteFG;
+    accent = Theme.of(context).brightness == Brightness.dark
+        ? colors.accent
+        : colors.accentL;
+    mainText = Theme.of(context).brightness == Brightness.dark
+        ? colors.darkmaintext
+        : colors.litemaintext;
+    subText = Theme.of(context).brightness == Brightness.dark
+        ? colors.darksubtext
+        : colors.litesubtext;
+
+    return Container(
+      color: background,
+      child: FutureBuilder(
+          future: getHttp(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: Text('Please wait its loading...'));
+            } else {
+              if (snapshot.hasError)
+                return Center(child: Text('Error: ${snapshot.error}'));
+              else
+                return ListView.builder(
+                    // physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: cats.iposNewsList.length,
+                    itemBuilder: (context, index) {
+                      return newsWidget(mainText, subText, foreground, newsList,
+                          index, context);
+                    });
+            }
+          }),
+    );
+  }
+}
--- lib/widgets/newswidget.dart
+++ lib/widgets/newswidget.dart
@@ -53,3 +53,61 @@ Widget newsWidget(Color mainText, Color subText, Color foreground, iposNewsList,
     ),
   );
 }
+
+Widget newsWidgets(Color mainText, Color subText, Color foreground,
+    iposNewsList, int index, BuildContext context) {
+  var source = iposNewsList[index]['source']['name'];
+  var title = iposNewsList[index]['title'];
+  var description = iposNewsList[index]['description'];
+  var urlToImage = iposNewsList[index]['urlToImage'];
+  var url = iposNewsList[index]['url'];
+  var publishedAt = iposNewsList[index]['publishedAt'];
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+    child: GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Webopen(url)),
+        );
+      },
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width - 40,
+                  // borderRadius: BorderRadius.circular(8.0),
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Image.network(
+                      urlToImage,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 16, color: mainText),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+              child: Text(
+                source,
+                style: TextStyle(fontSize: 12, color: subText),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
